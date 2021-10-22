300bfcd60990a5baebb858cf62f42ecca0ef9aa9
CANNIBAL WH1YTE <90905815+wh1yterr@users.noreply.github.com>
2021-10-22 16:03:05 +0300

    Create 4_1.py

---
+++ 4/4_1.py
@@ -0,0 +1,16 @@
+# -- coding: utf-8 --
+s = input()
+print(':::::::')
+print('1 - ', s[2])
+x = int(len(s)) -2
+print('2 - ', s[x])
+print('3 - ', s[:5])
+print('4 - ', s[:x])
+x = int(len(s))
+print('5 - ', s[0:x:2])
+x = int(len(s))
+print('6 - ', s[1:x:2])
+print('7 - ', s[::-1])
+print('8 - ', s[::-2])
+x = len(s)
+print('9 - ', x)
